7c1338efb856bc14108620c863b206a98e92e844
doubleucoder <93402329+doubleucoder@users.noreply.github.com>
2022-10-10 17:51:27 +0530

    Create bubbleSort.c

---
+++ c/bubbleSort.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+ 
+void printArray(int* A, int n){
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", A[i]);
+    }
+    printf("\n");
+}
+void bubbleSort(int *A, int n){
+    int temp;
+    int isSorted = 0;
+    for (int i = 0; i < n-1; i++)
+    {
+        printf("Working on pass number %d\n", i+1); 
+        for (int j = 0; j <n-1-i ; j++)
+        {
+            if(A[j]>A[j+1]){
+                temp = A[j];
+                A[j] = A[j+1];
+                A[j+1] = temp; 
+            }
+        }  
+    } 
+}
+ 
+void bubbleSortAdaptive(int *A, int n){
+    int temp;
+    int isSorted = 0;
+    for (int i = 0; i < n-1; i++)
+    {
+        printf("Working on pass number %d\n", i+1);
+        isSorted = 1;
+        for (int j = 0; j <n-1-i ; j++)
+        {
+            if(A[j]>A[j+1]){
+                temp = A[j];
+                A[j] = A[j+1];
+                A[j+1] = temp;
+                isSorted = 0;
+            }
+        } 
+        if(isSorted){
+            return;
+        }
+    } 
+}
+ 
+int main(){
+    int A[] = {1, 2, 5, 6, 12, 54, 625, 7, 23, 9, 987};
+    int n = 11;
+    printArray(A, n);
+    bubbleSort(A, n);
+    printArray(A, n);
+    return 0;
+}
